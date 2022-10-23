1f8e92389d0764f2861ec410182eedc3695e7824
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-10-23 12:53:49 -0300

    Find user by id

--- backend/src/main/java/com/devsuperior/movieflix/services/UserService.java
+++ backend/src/main/java/com/devsuperior/movieflix/services/UserService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.movieflix.services;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +11,10 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.movieflix.dto.UserDTO;
 import com.devsuperior.movieflix.entities.User;
 import com.devsuperior.movieflix.repositories.UserRepository;
+import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService implements UserDetailsService{
@@ -27,6 +31,13 @@ public class UserService implements UserDetailsService{
 	public User getProfile() {
 		User user = authService.authenticated();
 		return user;
+	}
+	
+	@Transactional(readOnly = true)
+	public UserDTO findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new UserDTO(entity);
 	}
 
 	@Override
