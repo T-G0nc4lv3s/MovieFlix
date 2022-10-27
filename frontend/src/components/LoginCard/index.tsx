581faaab76ec5e07c5860fcb5290ee819858da5d
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-10-27 19:10:23 -0300

    LoginCard layout and responsiveness

--- frontend/src/components/LoginCard/index.tsx
+++ frontend/src/components/LoginCard/index.tsx
@@ -1,11 +1,18 @@
-
+import './styles.css';
 
 const LoginCard = () => {
-
-    return (
-        <>
-        </>
-    );
-}
+  return (
+    <div className="login-card-container">
+      <form>
+        <div className="form-container">
+          <label htmlFor="">LOGIN</label>
+          <input type="text" id="ctrl-email" placeholder="Email" />
+          <input type="text" id="ctrl-password" placeholder="Senha" />
+          <input type="submit" value="FAZER LOGIN" className="btn btn-primary btn-login" />
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default LoginCard;
