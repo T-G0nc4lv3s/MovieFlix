a68582e0b89fad6c814d71ffdd4232ef31fd5756
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-10-27 20:26:10 -0300

    Home layout and responsiveness

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,14 +1,24 @@
 import './styles.css';
+import Banner from 'assets/images/main.png';
+import LoginCard from 'components/LoginCard';
 
 const Home = () => {
   return (
     <>
       <div className="home-container">
         <div className="banner-container">
-          <h1>Banner</h1>
+          <div className="header-container">
+            <h1>Avalie Filmes</h1>
+          </div>
+          <div className="paragraph-container">
+            <p>Diga o que você achou do seu filme favorito</p>
+          </div>
+          <div className="img-container">
+            <img src={Banner} alt="Imagem banner" />
+          </div>
         </div>
         <div className="card-container">
-          <h1>Login Card</h1>
+          <LoginCard />
         </div>
       </div>
     </>
