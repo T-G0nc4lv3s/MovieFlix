60621d1e65d4c543902b362b1d6d285c5a0594f9
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-10-28 21:47:19 -0300

    React hook form

--- frontend/src/components/LoginCard/index.tsx
+++ frontend/src/components/LoginCard/index.tsx
@@ -1,14 +1,45 @@
 import './styles.css';
+import { useForm } from 'react-hook-form';
+
+
+type FormData = {
+  username: string;
+  password: string;
+}
 
 const LoginCard = () => {
+
+  const { register, handleSubmit } = useForm<FormData>();
+
+  const onSubmit = (formData: FormData) => {
+    console.log(formData);
+    
+  }
+
   return (
     <div className="login-card-container">
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="form-container">
           <label htmlFor="">LOGIN</label>
-          <input type="text" id="ctrl-email" placeholder="Email" />
-          <input type="text" id="ctrl-password" placeholder="Senha" />
-          <input type="submit" value="FAZER LOGIN" className="btn btn-primary btn-login" />
+          <input
+            {...register('username')}
+            type="text"
+            name="username"
+            id="ctrl-email"
+            placeholder="Email"
+          />
+          <input
+            {...register('password')}
+            type="text"
+            name="password"
+            id="ctrl-password"
+            placeholder="Senha"
+          />
+          <input
+            type="submit"
+            value="FAZER LOGIN"
+            className="btn btn-primary btn-login"
+          />
         </div>
       </form>
     </div>
