241dabc659ca8a3e78ea240f456ee4a645f9ad24
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-10-31 20:04:59 -0300

    Form validation

--- frontend/src/components/LoginCard/index.tsx
+++ frontend/src/components/LoginCard/index.tsx
@@ -11,7 +11,11 @@ type FormData = {
 const LoginCard = () => {
   const [hasError, setHasError] = useState(false);
 
-  const { register, handleSubmit } = useForm<FormData>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
   const onSubmit = (formData: FormData) => {
     requestBackendLogin(formData)
@@ -31,22 +35,38 @@ const LoginCard = () => {
         <div className="form-container">
           <label htmlFor="">LOGIN</label>
           {hasError && (
-            <div className="alert alert-danger">Erro ao tentar efetuar login</div>
+            <div className="alert alert-danger">
+              Erro ao tentar efetuar login
+            </div>
           )}
           <input
-            {...register('username')}
+            {...register('username', {
+              required: 'Campo requerido',
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: 'Email inválido',
+              },
+            })}
             type="text"
             name="username"
             id="ctrl-email"
             placeholder="Email"
           />
+          <div className="invalid-feedback d-block">
+            {errors.username?.message}
+          </div>
           <input
-            {...register('password')}
+            {...register('password', {
+              required: 'Campo requerido',
+            })}
             type="password"
             name="password"
             id="ctrl-password"
             placeholder="Senha"
           />
+          <div className="invalid-feedback d-block">
+            {errors.password?.message}
+          </div>
           <input
             type="submit"
             value="FAZER LOGIN"
