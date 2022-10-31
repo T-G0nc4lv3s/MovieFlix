9f416a13a1620ef0daf2155f89ae06acb7145cff
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-10-30 21:19:56 -0300

    Manual error handling

--- frontend/src/components/LoginCard/index.tsx
+++ frontend/src/components/LoginCard/index.tsx
@@ -1,32 +1,38 @@
 import './styles.css';
 import { useForm } from 'react-hook-form';
+import { useState } from 'react';
 import { requestBackendLogin } from 'utils/requests';
-
 
 type FormData = {
   username: string;
   password: string;
-}
+};
 
 const LoginCard = () => {
+  const [hasError, setHasError] = useState(false);
 
   const { register, handleSubmit } = useForm<FormData>();
 
   const onSubmit = (formData: FormData) => {
     requestBackendLogin(formData)
-    .then(response => {
-      console.log('Sucesso ', response); 
-    })
-    .catch(error => {
-      console.log('Erro ' + error);
-    });
-  }
+      .then((response) => {
+        setHasError(false);
+        console.log('Sucesso ', response);
+      })
+      .catch((error) => {
+        setHasError(true);
+        console.log('Erro ' + error);
+      });
+  };
 
   return (
     <div className="login-card-container">
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="form-container">
           <label htmlFor="">LOGIN</label>
+          {hasError && (
+            <div className="alert alert-danger">Erro ao tentar efetuar login</div>
+          )}
           <input
             {...register('username')}
             type="text"
