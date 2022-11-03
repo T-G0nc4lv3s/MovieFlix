3319d3a68ef854795b8a04b581d64b452fec67e6
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-11-03 16:14:40 -0300

    Content restriction based on user roles

--- frontend/src/pages/MovieReviews/index.tsx
+++ frontend/src/pages/MovieReviews/index.tsx
@@ -1,6 +1,7 @@
 import './styles.css';
 import ReviewSubmit from 'components/ReviewSubmit';
 import ReviewListCard from 'components/ReviewListCard';
+import { hasAnyRoles } from 'utils/requests';
 
 const MovieReviews = () => {
   return (
@@ -8,9 +9,11 @@ const MovieReviews = () => {
       <div className="card-header-container">
         <h1>Tela detalhes do filme id: 1</h1>
       </div>
-      <div className="form-review-container">
-        <ReviewSubmit />        
-      </div>
+      {hasAnyRoles(['ROLE_MEMBER']) && (
+        <div className="form-review-container">
+          <ReviewSubmit />
+        </div>
+      )}
       <div className="review-list-container">
         <ReviewListCard />
       </div>
