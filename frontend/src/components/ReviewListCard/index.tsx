182ade01ede9b0b1eee26340e497335223a679c6
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-11-03 12:57:23 -0300

    Reviews list request

--- frontend/src/components/ReviewListCard/index.tsx
+++ frontend/src/components/ReviewListCard/index.tsx
@@ -1,30 +1,40 @@
 import './styles.css';
 import Star from 'assets/images/star.svg';
+import { Review } from 'types/review';
+import { useState, useEffect } from 'react';
+import { requestBackend } from 'utils/requests';
+import { AxiosRequestConfig } from 'axios';
 
 const ReviewListCard = () => {
+  const [list, setList] = useState<Review[]>();
+
+  useEffect(() => {
+    const params: AxiosRequestConfig = {
+      method: 'GET',
+      url: '/movies/1/reviews',
+      withCredentials: true,
+    };
+
+    requestBackend(params).then((response) => {
+      setList(response.data);
+    });
+  }, []);
+
   return (
     <div className="list-card-container container-fluid">
-      <div className="username-container">
-        <img src={Star} alt="ícone" />
-        <p>Maria Silva</p>
-      </div>
-      <div className="review-text-container">
-        <p>Gostei muito do filme. Foi muito bom mesmo. Pena que durou pouco</p>
-      </div>
-      <div className="username-container">
-        <img src={Star} alt="ícone" />
-        <p>Maria Silva</p>
-      </div>
-      <div className="review-text-container">
-        <p>Gostei muito do filme. Foi muito bom mesmo. Pena que durou pouco</p>
-      </div>
-      <div className="username-container">
-        <img src={Star} alt="ícone" />
-        <p>Maria Silva</p>
-      </div>
-      <div className="review-text-container">
-        <p>Gostei muito do filme. Foi muito bom mesmo. Pena que durou pouco</p>
-      </div>
+      {list?.map((review) => {
+        return (
+          <div key={review.id}>
+            <div className="username-container">
+              <img src={Star} alt="ícone" />
+              <p>{review.user.name}</p>
+            </div>
+            <div className="review-text-container">
+              <p>{review.text}</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
