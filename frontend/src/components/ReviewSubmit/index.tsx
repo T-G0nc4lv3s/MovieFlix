1bc1ae0bae76624951c074482b04f06908f65461
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-11-05 16:08:09 -0300

    Hook form to ReviewSubmit

--- frontend/src/components/ReviewSubmit/index.tsx
+++ frontend/src/components/ReviewSubmit/index.tsx
@@ -1,15 +1,30 @@
 import './styles.css';
+import { useForm } from 'react-hook-form';
+
+type FormData = {
+    text : string
+}
 
 const ReviewSubmit = () => {
+
+
+  const { register, handleSubmit } = useForm<FormData>();
+
+  const onSubmit = (formData: FormData) => {
+    console.log(formData);
+  }
+
   return (
     <div className="review-submit-card">
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="form-review-container">
           <div className="input-review-container">
             <input
+            {...register('text')}
               type="text"
               id="ctrl-review"
               placeholder="Deixe sua avaliação aqui"
+              name="text"
             />
           </div>
           <div className="input-btn-container">
