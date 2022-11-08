75086140fa8ba7613ac684a3ae46b1ce0fdee58c
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-11-08 20:33:56 -0300

    ReviewSubmit refactor

--- frontend/src/components/ReviewSubmit/index.tsx
+++ frontend/src/components/ReviewSubmit/index.tsx
@@ -1,18 +1,39 @@
 import './styles.css';
 import { useForm } from 'react-hook-form';
+import { requestBackend } from 'utils/requests';
+import { AxiosRequestConfig } from 'axios';
+import { Review } from 'types/review';
 
 type FormData = {
-    text : string
-}
+  text: string;
+};
 
-const ReviewSubmit = () => {
+type Props = {
+  movieId: string;
+  onInsertReview: (review: Review) => void;
+};
 
-
-  const { register, handleSubmit } = useForm<FormData>();
+const ReviewSubmit = ({ movieId, onInsertReview }: Props) => {
+  const { register, handleSubmit, setValue } = useForm<FormData>();
 
   const onSubmit = (formData: FormData) => {
+    const params: AxiosRequestConfig = {
+      method: 'post',
+      url: '/reviews',
+      withCredentials: true,
+      data: {
+        text: formData.text,
+        movieId: movieId,
+      },
+    };
+
+    requestBackend(params).then((response) => {
+      onInsertReview(response.data);
+    });
+    setValue('text', '');
+
     console.log(formData);
-  }
+  };
 
   return (
     <div className="review-submit-card">
@@ -20,7 +41,7 @@ const ReviewSubmit = () => {
         <div className="form-review-container">
           <div className="input-review-container">
             <input
-            {...register('text')}
+              {...register('text')}
               type="text"
               id="ctrl-review"
               placeholder="Deixe sua avaliação aqui"
