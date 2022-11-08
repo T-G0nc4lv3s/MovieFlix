999f5b82571f17ae74d9b5aff7ea4e3a1648fb83
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-11-08 14:48:38 -0300

    MovieList refactor

--- frontend/src/pages/MovieList/index.tsx
+++ frontend/src/pages/MovieList/index.tsx
@@ -1,45 +1,42 @@
 import './styles.css';
-import { useState, useEffect } from 'react';
 import { AxiosRequestConfig } from 'axios';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import { Movie } from 'types/movie';
-import { requestBackend } from 'utils/requests';
 import { SpringPage } from 'types/vendor/spring';
+import { requestBackend } from './../../utils/requests';
+import MovieCard from 'components/MovieCard';
+import { Link } from 'react-router-dom';
 
 const MovieList = () => {
-
   const [page, setPage] = useState<SpringPage<Movie>>();
 
-  useEffect(() => {
+  const params: AxiosRequestConfig = {
+    method: 'GET',
+    url: '/movies',
+    withCredentials: true,
+    params: {
+      genreId: 0,
+    },
+  };
 
-    const params : AxiosRequestConfig = {
-        method: 'GET',
-        url: '/movies',
-        withCredentials: true,
-        params: {
-            genreId: 0,
-        }
-    }
-    requestBackend(params)
-    .then(response => {
-        setPage(response.data);
-        console.log(page);  
-    })
-  }, [])
+  useEffect(() => {
+    requestBackend(params).then((response) => {
+      setPage(response.data);
+    });
+  }, []);
 
   return (
     <div className="movie-list-container">
       <h1>Tela listagem de filmes</h1>
-      <div>
-        {page?.content.map(movie => {
-
-            return (
-                <span className="movie-link" key={movie.id}>
-                    <Link to="/movies/:movieId">Acessar /movies/{movie.id}</Link>
-                </span>
-            );
-        })}
-      </div>
+      {page?.content.map((movie) => {
+        return (
+          <div className="movie-link" key={movie.id}>
+            <Link to={`movies/${movie.id}`}> 
+              <MovieCard movie={movie} />
+            </Link>
+          </div>
+        );
+      })}
     </div>
   );
 };
