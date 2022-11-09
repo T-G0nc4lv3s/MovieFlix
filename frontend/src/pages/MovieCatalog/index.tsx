7ee199f5d5fcda7866121d3efcbc0b2bb5ee1ad2
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-11-08 23:11:58 -0300

    Catalog loader

--- frontend/src/pages/MovieCatalog/index.tsx
+++ frontend/src/pages/MovieCatalog/index.tsx
@@ -6,8 +6,11 @@ import { SpringPage } from 'types/vendor/spring';
 import { requestBackend } from './../../utils/requests';
 import MovieCard from 'components/MovieCard';
 import { Link } from 'react-router-dom';
+import ThreeDotsLoader from 'pages/MovieCatalog/ThreeDotsLoader';
 
 const MovieList = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
   const [page, setPage] = useState<SpringPage<Movie>>();
 
   const params: AxiosRequestConfig = {
@@ -20,23 +23,33 @@ const MovieList = () => {
   };
 
   useEffect(() => {
-    requestBackend(params).then((response) => {
-      setPage(response.data);
-    });
+    setIsLoading(true);
+    requestBackend(params)
+      .then((response) => {
+        setPage(response.data);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
 
   return (
     <div className="movie-list-container">
       <h1>Tela listagem de filmes</h1>
-      {page?.content.map((movie) => {
-        return (
+      {isLoading ? (
+        <>
+          <h1>Carregando</h1>
+          <ThreeDotsLoader />
+        </>
+      ) : (
+        page?.content.map((movie) => (
           <div className="movie-link" key={movie.id}>
-            <Link to={`movies/${movie.id}`}> 
+            <Link to={`movies/${movie.id}`}>
               <MovieCard movie={movie} />
             </Link>
           </div>
-        );
-      })}
+        ))
+      )}
     </div>
   );
 };
